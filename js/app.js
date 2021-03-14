8d3f4086db44b66387f2b6c5186a420e48d45449
wafaankoush99 <wafadirawe@gmail.com>
2021-03-14 16:08:10 +0200

    w

--- js/app.js
+++ js/app.js
@@ -1,152 +1,118 @@
 'use strict';
 
-// const frankie = {
-//   name: 'Frankie',
-
-//   age: 0,
-//   getAge: function( min, max ) {
-//     this.age = generateRandomNumber( min, max );
-//   },
-
-//   likes: ['napping', 'chasing', 'food'],
-//   isGoodWithKids: true,
-//   isGoodWithDogs: false,
-//   isGoodWithOtherCat: true,
-//   breed: 'British',
-//   image: './images/frankie.jpeg',
-//   render: function() {
-//     const parentElement = document.getElementById( 'kittenProfiles' );
-
-//     // Create Element
-//     // append element to parent element
-//     // set text content to the element
-
-//     const articleElement = document.createElement( 'article' );
-//     parentElement.appendChild( articleElement );
-
-//     const h2Element = document.createElement( 'h2' );
-//     articleElement.appendChild( h2Element );
-//     h2Element.textContent = this.name;
-
-//     const pElement = document.createElement( 'p' );
-//     articleElement.appendChild( pElement );
-//     pElement.textContent = `${this.name} is a ${this.breed}, and ${this.age} months old.`;
-
-//     const ulElement = document.createElement( 'ul' );
-//     articleElement.appendChild( ulElement );
-
-//     for( let i = 0; i < this.likes.length; i++ ) {
-//       const liElement = document.createElement( 'li' );
-//       ulElement.appendChild( liElement );
-//       liElement.textContent = this.likes[i];
-//     }
-
-//     const imgElement = document.createElement( 'img' );
-//     articleElement.appendChild( imgElement );
-//     imgElement.setAttribute( 'src', this.image );
-//   }
-
-// };
-
-// // to get the random number you must follow these steps:
-
-// // [1] invoke function *(must to be before [2] step)*
-// frankie.getAge( 10,15 );
-// // [2] invoke property
-// console.log( frankie.age );
-// // [3] invoke object
-// console.log( frankie );
-
-// frankie.render();
-
-
-
 let hourArr = ['06-AM', '07-AM', '08-AM', '09-AM', '10-AM', '11-AM'];
 
-let seattle = {
-  name: 'Seattle',
-  minCus: 23,
-  maxCus: 65,
-  avgCus: 6.3,
-  randomMinMax: [],
-  randomAvg: [],
-  total: 0,
+function Country ( name, minCus, maxCus, avgCus ){
+  this.name = name,
+  this.minCus = minCus,
+  this.maxCus = maxCus,
+  this.avgCus = avgCus,
+  this.randomMinMax = [],
+  this.randomAvg = [];
+  this.total = 0;
+}
 
-  getMinPlusMax: function( min, max ){
-    for ( let i =0; i < hourArr.length; i++ ) {
-      this.randomMinMax[i] = generateRandomNumber( min, max );
-    //   console.log( this.randomMinMax );
-    }
-  },
 
-  getAvgNum: function() {
-    for ( let i = 0 ; i < hourArr.length ; i++ ) {
-      this.randomAvg[i] = Math.ceil( this.randomMinMax[i] * this.avgCus );
+Country.prototype.getAvgNum = function() {
+  for ( let i = 0 ; i < hourArr.length ; i++ ) {
+    let piece = Math.ceil( generateRandomNumber( this.minCus, this.maxCus ) * this.avgCus );
+    this.randomAvg.push( piece );
 
-    }
-  },
+  } };
 
-  getTotal: function() {
-    for ( let i in this.randomAvg )
-      this.total += this.randomAvg[i];
-    // console.log( this.total );
+Country.prototype.totalSales = function() {
+  for ( let i in this.randomAvg ) {
+    this.total += this.randomAvg[i];
+    console.log( this.total ); }
+};
 
-  },
+Country.prototype.render = function() {
 
+  const myTable = document.getElementById( 'myTable' );
+
+  const dataRows = document.createElement( 'tr' );
+  myTable.appendChild( dataRows );
+
+  const namesTd = document.createElement( 'th' );
+  dataRows.appendChild( namesTd );
+  namesTd.textContent = this.name;
+
+  for ( let i = 0 ; i < hourArr.length; i++ ) {
+    const dataTd = document.createElement( 'td' );
+    dataRows.appendChild( dataTd );
+    dataTd.textContent = `${this.randomAvg[i]}`;
+
+  }
+
+  let sumDaily = document.createElement( 'td' );
+  dataRows.appendChild( sumDaily );
+  sumDaily.textContent = `${this.total}`;
 
 
 };
 
-function getRender(){
+const tableHeader = function() {
   const parentElement = document.getElementById( 'myDiv' );
-  const myH2 = document.createElement( 'h2' );
-  parentElement.appendChild( myH2 );
-  myH2.textContent = seattle.name;
-  // console.log( myH2.textContent );
+  let myTable = document.createElement( 'table' );
+  parentElement.appendChild( myTable );
+  myTable.setAttribute( 'id','myTable' );
+  let tr1 = document.createElement( 'tr' );
+  myTable.appendChild( tr1 );
 
-  const myUl = document.createElement( 'ul' );
-  parentElement.appendChild( myUl );
+  let tdCountry = document.createElement( 'th' );
+  tr1.appendChild( tdCountry );
+  tdCountry.textContent = 'Country';
 
-  for ( let i=0; i < hourArr.length; i++ ){
-    const myLi = document.createElement( 'li' );
-    myUl.appendChild( myLi );
-    myLi.textContent = `${hourArr[i]} : ${seattle.randomAvg[i]} pieces.`;
-
+  for ( let i =0; i < hourArr.length; i++ ) {
+    const headerTh = document.createElement( 'th' );
+    tr1.appendChild( headerTh );
+    headerTh.textContent = `  ${hourArr[i]} `;
   }
 
-  const myP = document.createElement( 'p' );
-  parentElement.appendChild( myP );
-  myP.textContent = `Total: ${seattle.total}.`;
+  let dailyTotal = document.createElement( 'th' );
+  tr1.appendChild( dailyTotal );
+  dailyTotal.textContent = 'Daily Total';
 
-}
+};
 
-
-
-seattle.getMinPlusMax( seattle.minCus, seattle.maxCus );
-// console.log( seattle.randomMinMax );
-seattle.getAvgNum( seattle.avgCus );
-// console.log( seattle.randomAvg );
-seattle.getTotal();
-
-// seattle.getRender();
-getRender();
+tableHeader();
 
 
+const tableFooter = function() {
+  const parentElement = document.getElementById( 'myTable' );
+  let footerTr = document.createElement( 'tr' );
+  parentElement.appendChild( footerTr );
+
+  let totalTd = document.createElement( 'th' );
+  footerTr.appendChild( totalTd );
+  totalTd.textContent = 'Totals';
+
+  for ( let i=0; i < hourArr.length ; i++ ) {
+    let sumIndex = document.createElement( 'td' );
+    footerTr.appendChild( sumIndex );
+    sumIndex.textContent = ( seattle.randomAvg[i] + tokyo.randomAvg[i] + dubai.randomAvg[i] ) ;
+  }
+
+  let lastTotal = document.createElement( 'td' );
+  footerTr.appendChild( lastTotal );
+  lastTotal.textContent= ( seattle.total + tokyo.total + dubai.total );
+};
+
+const seattle = new Country ( 'Seattle', 4, 35, 2.1 );
+seattle.getAvgNum();
+seattle.totalSales();
+seattle.render();
+const tokyo = new Country ( 'Tokyo', 3, 30, 2.2 );
+tokyo.getAvgNum();
+tokyo.totalSales();
+tokyo.render();
+const dubai = new Country ( 'Dubai', 11, 38, 3.7 );
+dubai.getAvgNum();
+dubai.totalSales();
+dubai.render();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+tableFooter();
 
 
 function generateRandomNumber( min, max ) {
